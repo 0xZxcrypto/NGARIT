5f8b8afddf1b79127e4ed4392c0eca00f68c85a5
0xzxcrypto <rizalsuzune@gmail.com>
2025-01-27 18:16:27 +0700

    Menambahkan file ke repositori

---
+++ src/ProxyManager.js
@@ -0,0 +1,41 @@
+require('colors');
+const axios = require('axios');
+const fs = require('fs');
+
+const PROXY_SOURCES = {
+  'SERVER 1': 'https://files.ramanode.top/airdrop/grass/server_1.txt',
+  'SERVER 2': 'https://files.ramanode.top/airdrop/grass/server_2.txt',
+  'SERVER 3': 'https://files.ramanode.top/airdrop/grass/server_3.txt',
+  'SERVER 4': 'https://files.ramanode.top/airdrop/grass/server_4.txt',
+  'SERVER 5': 'https://files.ramanode.top/airdrop/grass/server_5.txt',
+  'SERVER 6': 'https://files.ramanode.top/airdrop/grass/server_6.txt',
+};
+
+async function fetchProxies(url) {
+  try {
+    const response = await axios.get(url);
+    console.log(`\nFetched proxies from ${url}`.green);
+    return response.data.split('\n').filter(Boolean);
+  } catch (error) {
+    console.error(`Failed to fetch proxies from ${url}: ${error.message}`.red);
+    return [];
+  }
+}
+
+async function readLines(filename) {
+  try {
+    const data = await fs.promises.readFile(filename, 'utf-8');
+    console.log(`Loaded data from ${filename}`.green);
+    return data.split('\n').filter(Boolean);
+  } catch (error) {
+    console.error(`Failed to read ${filename}: ${error.message}`.red);
+    return [];
+  }
+}
+
+async function selectProxySource(inquirer) {
+  // Langsung memilih 'NO PROXY'
+  return { type: 'none' };
+}
+
+module.exports = { fetchProxies, readLines, selectProxySource };
