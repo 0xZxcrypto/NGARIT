787840bd64a201b285a97805218bd87a4db4c347
0xZxcrypto <74732301+0xZxcrypto@users.noreply.github.com>
2025-01-27 18:23:26 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ const delay = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 function displayHeader() {
     process.stdout.write('\x1Bc');
     console.log('----------------------------------------'.cyan);
-    console.log('=          Grass Airdrop Bot            ='.cyan);
+    console.log('=            Grass No Proxy             ='.cyan);
     console.log('=       Developed by 0xZxcrypto         ='.cyan);
     console.log('=     https://github.com/0xZxcrypto     ='.cyan);
     console.log('----------------------------------------'.cyan);
